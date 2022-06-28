cfce462ec5d75e6dadd91f5697d6993e4146a802
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-06-28 09:54:28 -0500

    commit

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ class Plant {
         this.climate = climate;
     }
     describe () {
-        return ' $(this.commonName) also known as ${this.scientificName} needs ${sunlight} and ${water} and lives in a ${climate} climate';
+        return `${this.commonName} also known as ${this.scientificName} needs ${sunlight} and ${water} and lives in a ${climate} climate`;
+    }
+}
+
+class TypeOfPlant {
+    constructor (type) {
+        this.type = type;
+        this.plant = [];
+    }
+    addPlant(plant) {
+        if (plant instanceof Plant) {
+            this.plants.push(plant);
+        } else {
+            throw new Error (`Argument is not a plant: ${plant}`);
+        }
+    }
+    describe() {
+        return `${this.type} has ${this.plants.length} plants.`;
     }
 }
